a0199fdbc74e2bfebb29f7e286adfd4595d18dea
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-16 08:02:37 +0200

    Update root outputs.tf

--- VPC_Basics_4_GWLB/Terraform/outputs.tf
+++ VPC_Basics_4_GWLB/Terraform/outputs.tf
@@ -22,3 +22,18 @@ output "public_subnet_id" {
   description = "Provider public subnet ID"
   value       = module.provider_vpc.public_subnet_id
 }
+
+output "consumer_vpc_id" {
+  description = "Consumer VPC ID"
+  value       = module.consumer_vpc.vpc_id
+}
+
+output "app_instance_id" {
+  description = "App EC2 instance ID"
+  value       = module.consumer_vpc.app_instance_id
+}
+
+output "app_instance_public_ip" {
+  description = "App EC2 public IP (use to SSH)"
+  value       = module.consumer_vpc.app_instance_public_ip
+}
