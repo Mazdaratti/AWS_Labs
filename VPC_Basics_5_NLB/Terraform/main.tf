9a72e71ae531b7b928746379b6eb47836cac6790
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-17 18:01:23 +0200

    Update root main.tf with nat_gateway module

--- VPC_Basics_5_NLB/Terraform/main.tf
+++ VPC_Basics_5_NLB/Terraform/main.tf
@@ -13,3 +13,14 @@ module "vpc" {
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
 }
+
+# =====================
+# NAT Gateway Module
+# =====================
+module "nat_gateway" {
+  source              = "./modules/nat_gateway"
+  vpc_id              = module.vpc.vpc_id
+  vpc_name            = var.vpc_name
+  public_subnet_id    = module.vpc.public_subnet_ids[0]
+  private_subnet_ids  = module.vpc.private_subnet_ids
+}
