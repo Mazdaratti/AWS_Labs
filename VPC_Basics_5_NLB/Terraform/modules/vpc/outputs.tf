1e0b3b9c7b58b22619c4ee2606e5326b860591d0
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-17 20:14:37 +0200

    Delete unused variable

--- VPC_Basics_5_NLB/Terraform/modules/vpc/outputs.tf
+++ VPC_Basics_5_NLB/Terraform/modules/vpc/outputs.tf
@@ -23,7 +23,4 @@ output "public_route_table_id" {
   value       = aws_route_table.public_rt.id
 }
 
-output "private_route_table_id" {
-  description = "ID of the private route table"
-  value       = aws_route_table.private_rt.id
-}
+
