29d6da365b43c133e277a496024146874ddf8627
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-17 11:42:42 +0200

    Create module vpc outputs.tf

---
+++ VPC_Basics_5_NLB/Terraform/modules/vpc/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "igw_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.gw.id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public_rt.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table"
+  value       = aws_route_table.private_rt.id
+}
