943ddf267880f9c6f05d25d0b9382ddcc69b5107
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 14:08:27 +0200

    Add outputs from security_groups module

--- VPC_Basics_6_PrivateLink/Terraform/outputs.tf
+++ VPC_Basics_6_PrivateLink/Terraform/outputs.tf
@@ -12,3 +12,13 @@ output "route_table_id" {
   description = "Private Route Table ID"
   value       = module.vpc.route_table_id
 }
+
+output "ec2_sg_id" {
+  description = "EC2 instance SG"
+  value       = module.security_groups.ec2_sg_id
+}
+
+output "endpoint_sg_id" {
+  description = "Endpoint SG"
+  value       = module.security_groups.endpoint_sg_id
+}
