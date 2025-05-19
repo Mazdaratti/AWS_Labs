8892777a9fce81b616fd919f2989aeeaf2ef6a6b
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 14:01:35 +0200

    Create root variables.tf

---
+++ VPC_Basics_6_PrivateLink/Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
