63c751c479701b384ca4e8db910b0c4cd16fc541
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 13:59:25 +0200

    Create module vpc main.tf

---
+++ VPC_Basics_6_PrivateLink/Terraform/modules/vpc/main.tf
@@ -0,0 +1,42 @@
+# =====================
+# VPC
+# =====================
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.vpc_name}"
+  }
+}
+
+# =====================
+# Private Subnet
+# =====================
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet"
+  }
+}
+
+# =====================
+# Private Route Table
+# =====================
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
