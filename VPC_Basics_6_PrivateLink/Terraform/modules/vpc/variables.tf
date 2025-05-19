ef80195849ba4e6b3e50cc009da539e2a161f4ba
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 13:59:50 +0200

    Create module vpc variables.tf

---
+++ VPC_Basics_6_PrivateLink/Terraform/modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_name" {
+  description = "Name tag for the VPC and subnet"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
