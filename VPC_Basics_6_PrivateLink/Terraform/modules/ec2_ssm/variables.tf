d0230166b681a3609be326814c27a1f97ade9652
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 15:10:29 +0200

    Create variables.tf for ec2_ssm module

---
+++ VPC_Basics_6_PrivateLink/Terraform/modules/ec2_ssm/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Prefix for naming resources"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to launch the EC2 into"
+  type        = string
+}
+
+variable "ec2_sg_id" {
+  description = "Security Group ID to associate with EC2"
+  type        = string
+}
