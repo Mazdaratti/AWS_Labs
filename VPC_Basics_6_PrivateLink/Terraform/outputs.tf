320d068b85c34fcb42664375f0f0ffc7def21e25
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 14:53:48 +0200

    Outputs for s3 and enpoint modules added

--- VPC_Basics_6_PrivateLink/Terraform/outputs.tf
+++ VPC_Basics_6_PrivateLink/Terraform/outputs.tf
@@ -22,3 +22,28 @@ output "endpoint_sg_id" {
   description = "Endpoint SG"
   value       = module.security_groups.endpoint_sg_id
 }
+
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = module.s3.bucket_name
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = module.s3.bucket_arn
+}
+
+output "s3_endpoint_id" {
+  description = "Gateway endpoint for S3"
+  value       = module.endpoints.s3_endpoint_id
+}
+
+output "ssm_endpoint_id" {
+  description = "Interface endpoint for SSM"
+  value       = module.endpoints.ssm_endpoint_id
+}
+
+output "ssmmessages_endpoint_id" {
+  description = "Interface endpoint for SSM Messages"
+  value       = module.endpoints.ssmmessages_endpoint_id
+}
