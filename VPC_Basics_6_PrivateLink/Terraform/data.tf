6309d303b5b61627f0685051d9dfd4977951fdc3
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 11:30:08 +0200

    Create data.tf

---
+++ VPC_Basics_6_PrivateLink/Terraform/data.tf
@@ -0,0 +1,36 @@
+# Latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+# Available AZs in the region
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Current AWS caller identity (for bucket naming)
+data "aws_caller_identity" "current" {}
+
+# AWS region (for endpoint configuration)
+data "aws_region" "current" {}
