1ce68a94e2c5a086736a11d20e1a97d8834a6bc0
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 15:12:04 +0200

    Add variable for instance type

--- VPC_Basics_6_PrivateLink/Terraform/variables.tf
+++ VPC_Basics_6_PrivateLink/Terraform/variables.tf
@@ -22,3 +22,9 @@ variable "bucket_name" {
   description = "Globally unique name for the S3 bucket"
   type        = string
 }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
