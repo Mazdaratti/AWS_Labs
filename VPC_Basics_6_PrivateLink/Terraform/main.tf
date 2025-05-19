e7c2313057b4d57e76abe173d6916963e977ce58
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-19 14:07:51 +0200

    Update root main.tf with security_groups module

--- VPC_Basics_6_PrivateLink/Terraform/main.tf
+++ VPC_Basics_6_PrivateLink/Terraform/main.tf
@@ -14,11 +14,21 @@ provider "aws" {
 }
 
 # =====================
-# VPC Module
+# --- VPC Module ---
 # =====================
 module "vpc" {
   source       = "./modules/vpc"
   vpc_name     = var.vpc_name
   vpc_cidr     = var.vpc_cidr
+  subnet_cidr  = var.subnet_cidr
+}
+
+# =====================
+# -- Security Groups --
+# =====================
+module "security_groups" {
+  source       = "./modules/security_groups"
+  vpc_name     = var.vpc_name
+  vpc_id       = module.vpc.vpc_id
   subnet_cidr  = var.subnet_cidr
 }
