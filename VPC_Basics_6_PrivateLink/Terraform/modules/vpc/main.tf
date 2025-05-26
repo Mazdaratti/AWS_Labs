a5875be2627ebe844b09adcb21ba3a5d1286dfd3
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-26 22:15:11 +0200

    Improve syntax

--- VPC_Basics_6_PrivateLink/Terraform/modules/vpc/main.tf
+++ VPC_Basics_6_PrivateLink/Terraform/modules/vpc/main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = false
 
   tags = {
