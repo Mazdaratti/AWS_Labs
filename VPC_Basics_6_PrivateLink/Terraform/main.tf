f96a88c7ded6876f4e287ad057d0e4d436eafbdb
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-27 15:41:09 +0200

    Implement endpoints module in root main.tf

--- VPC_Basics_6_PrivateLink/Terraform/main.tf
+++ VPC_Basics_6_PrivateLink/Terraform/main.tf
@@ -50,9 +50,8 @@ module "iam" {
 # =====================
 module "endpoints" {
   source          = "./modules/endpoints"
-  vpc_name        = var.vpc_name
   vpc_id          = module.vpc.vpc_id
-  subnet_id       = module.vpc.private_subnet_id
+  subnet_ids      = [module.vpc.private_subnet_id]
   route_table_id  = module.vpc.private_route_table_id
   endpoint_sg_id  = module.security_groups.endpoint_sg_id
   region          = var.region
