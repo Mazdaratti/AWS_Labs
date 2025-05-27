abf88e820c207e6f3da9fd78ade8dbc0244221e1
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-27 09:52:22 +0200

    Rename ec2 module

--- VPC_Basics_6_PrivateLink/Terraform/modules/ec2_ssm/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.private_ec2.id
-}
-
-output "instance_private_ip" {
-  description = "Private IP of the EC2 instance"
-  value       = aws_instance.private_ec2.private_ip
-}
