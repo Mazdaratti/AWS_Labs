af79a7ffe112eba621321500b3cbb680f431cc20
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-05-27 08:54:19 +0200

    Update vpc module
    Add public subnet

--- VPC_Basics_6_PrivateLink/Terraform/modules/vpc/main.tf
+++ VPC_Basics_6_PrivateLink/Terraform/modules/vpc/main.tf
@@ -12,17 +12,67 @@ resource "aws_vpc" "main" {
 }
 
 # =====================
+# Public Subnet
+# =====================
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet"
+  }
+}
+
+
+# =====================
 # Private Subnet
 # =====================
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidr
+  cidr_block              = var.private_subnet_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.vpc_name}-private-subnet"
   }
+}
+
+# =====================
+# Internet Gateway
+# =====================
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+# =====================
+# Public Route Table
+# =====================
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+# =========================
+# Public Subnet Association
+# =========================
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 # =====================
@@ -36,6 +86,9 @@ resource "aws_route_table" "private" {
   }
 }
 
+# =========================
+# Private Subnet Association
+# =========================
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
--- VPC_Basics_6_PrivateLink/Terraform/modules/vpc/outputs.tf
+++ VPC_Basics_6_PrivateLink/Terraform/modules/vpc/outputs.tf
@@ -3,12 +3,14 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
+output "public_subnet_id" {
+  description = "ID of the created private subnet"
+  value       = aws_subnet.public.id
+}
+
 output "private_subnet_id" {
   description = "ID of the created private subnet"
   value       = aws_subnet.private.id
 }
 
-output "route_table_id" {
-  description = "ID of the private route table"
-  value       = aws_route_table.private.id
-}
+
